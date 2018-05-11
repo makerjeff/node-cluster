f2cca62d520195eda472d6b3e4178be16d8d6089
Jeff Wu <makerjeffwu@gmail.com>
2018-05-10 22:25:11 -0700

    updated to use clusers

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // app.js
 
-// include express
-const express       = require('express');
+const cluster       = require('cluster');
 
-// create new Express app
-const app           = express();
+// cluster - check to see if we're on the master process
+if (cluster.isMaster) {
 
-// add a basic route - index page
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    // count the CPUs
+    let cpu_count = require('os').cpus().length;
 
-// bind to a port
-app.listen(3000, () => {
-    console.log(`Server started on port 3000`);
-});
+    // create worker for each CPU
+    for (let i = 0; i < cpu_count; i++) {
+        cluster.fork();
+    }
+
+} else {
+    // code to run if we're in a worker process
+
+    const express       = require('express');
+    const app           = express();
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+        res.send(`Hello world from Worker ${cluster.worker.id}`);
+    });
+    
+    app.listen(3000, () => {
+        console.log(`Worker Server started with ID ${cluster.worker.id}`);
+    });
+
+}
