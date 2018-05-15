2b6fd1d73fd6a92234887a97ca634d8ec122231e
Jeff Wu <makerjeffwu@gmail.com>
2018-05-15 15:58:41 -0700

    latest, working aplay module

--- sb-server2-aplay.js
+++ sb-server2-aplay.js
@@ -1,3 +1,16 @@
 // using NPM aplay package.
 // https://www.npmjs.com/package/aplay
 
+const Sound = require('aplay');
+
+
+let music = new Sound();
+
+let track_url = './audio/ok.wav';
+
+music.play(track_url);
+
+
+music.on('complete', () => {
+    console.log('music done playing. ');
+});
